b3cc49b5638a1c413ca70c7f2692b1afe9d81ec9
Skyler Parr <skylerparr@gmail.com>
2017-06-19 10:10:18 -0700

    fixed build warnings

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule Tail.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     description: description,
-     package: package,
+     deps: deps(),
+     description: description(),
+     package: package(),
      docs: [readme: "README.md",
             main: "README",
             source_url: "https://github.com/TheFirstAvenger/elixir-tail.git"]
